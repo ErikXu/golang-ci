463ecf0606d553272e4ad0da8f8be7117da495ff
ErikXu <81636759@qq.com>
2021-09-01 02:01:40 +0000

    Fix build issue.

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
+#!/bin/bash
+
 docker run --rm -i \
     -v /go:/go \
     -v ${PWD}:/workspace \
     golang:1.16-alpine \
-    sh -c 'cd /workspace && sh build.sh'
+    sh -c 'cd /workspace && sh publish.sh'
