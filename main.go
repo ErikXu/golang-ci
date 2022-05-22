a7de903f756f57c7171263c18e4cee9a27eb756f
Erik Xu <81636759@qq.com>
2022-05-23 01:43:51 +0800

    Update main.go

--- main.go
+++ main.go
@@ -9,5 +9,10 @@ func main() {
 			"message": "pong",
 		})
 	})
+	r.GET("/hello", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Hello ErikXu!",
+		})
+	})	
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
